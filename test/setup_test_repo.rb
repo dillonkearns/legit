5d0912cb1ce42f7902d3fba75d94b46ddc8fb0d0
Dillon Kearns <dillon@dillonkearns.com>
2013-08-30 07:40:51 -0700

    Turn off verbosity in SetupTestRepo to quiet test output

--- test/setup_test_repo.rb
+++ test/setup_test_repo.rb
@@ -6,22 +6,22 @@ class SetupTestRepo < Thor
   desc "SOMETHING", 'something else'
   def create_repo
     test_location = File.expand_path('../example_repo', __FILE__)
-    remove_dir(test_location)
+    remove_dir(test_location, :verbose => false, :capture => false)
     inside(test_location) do
-      run 'git init'
-      run 'git config user.name "John Doe" && git config user.email johndoe@example.com'
+      run 'git init', :verbose => false, :capture => false
+      run 'git config user.name "John Doe" && git config user.email johndoe@example.com', :verbose => false, :capture => false
       setup_branches
     end
   end
 
   private
   def setup_branches
-    create_file('README.txt', 'Example repo for testing')
-    run "git add ."
-    run "git commit -m 'Add README'"
+    create_file('README.txt', 'Example repo for testing', :verbose => false, :capture => false)
+    run "git add .", :verbose => false, :capture => false
+    run "git commit -m 'Add README'", :verbose => false, :capture => false
     branch_names = %w{ feature_with_unique_match multiple_matches_a multiple_matches_b UPPERCASE_BRANCH }
     branch_names.each do |branch_name|
-      run "git branch #{branch_name}"
+      run "git branch #{branch_name}", :verbose => false, :capture => false
     end
   end
 end
