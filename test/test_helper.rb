67a71e2c92be2b5f5faa99be39c68b58e354faf7
Dillon Kearns <dillon@dillonkearns.com>
2013-08-31 20:09:00 -0700

    Remove extraneous test output

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,3 +5,23 @@ Coveralls.wear!
 
 require 'minitest/autorun'
 require 'rr'
+
+require 'thor'
+# silence warnings for mocks
+class Thor
+  def self.create_command(meth) #:nodoc:
+    if @usage && @desc
+      base_class = @hide ? Thor::HiddenCommand : Thor::Command
+      commands[meth] = base_class.new(meth, @desc, @long_desc, @usage, method_options)
+      @usage, @desc, @long_desc, @method_options, @hide = nil
+      true
+    elsif self.all_commands[meth] || meth == "method_missing"
+      true
+    else
+      #puts "[WARNING] Attempted to create command #{meth.inspect} without usage or description. " <<
+      #         "Call desc if you want this method to be available as command or declare it inside a " <<
+      #         "no_commands{} block. Invoked from #{caller[1].inspect}."
+      false
+    end
+  end
+end
